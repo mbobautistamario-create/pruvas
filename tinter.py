59f6408bba6cb58357514d94ceab4542ebb7009a
Mario Ortega_117 <mbo.bautista.mario@gmail.com>
2026-03-11 20:17:02 -0300

    tkinter

---
+++ tinter.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+ventana = tk.Tk()          # crea la ventana
+ventana.title("Mi programa")  # titulo de la ventana
+ventana.geometry("400x300")   # tamaño
+
+ventana.mainloop()         # mantiene la ventana abierta
